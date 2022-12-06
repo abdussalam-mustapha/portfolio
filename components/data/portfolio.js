effbcd123f50b89dd3e7eed3ba38e22d1d374fad
abdussalam-mustapha <abdussalammustapha07@gmail.com>
2022-12-06 13:36:30 +0100

    new changes

---
+++ components/data/portfolio.js
@@ -0,0 +1,42 @@
+
+
+const portfolio = [
+    {
+       id: "1",
+       title: "My Portfolio Version 1",
+       category: "web app",
+       keyword: ["web app", "article", "Machine Learning"]
+    },
+    {
+       id: "2",
+       title: "My Portfolio Version 1",
+       category: "web app",
+       keyword: ["web app", "article", "Machine Learning"]
+    },
+    {
+       id: "3",
+       title: "My Portfolio Version 1",
+       category: "web app",
+       keyword: ["web app", "article", "Machine Learning"]
+    },
+    {
+       id: "4",
+       title: "My Portfolio Version 1",
+       category: "web app",
+       keyword: ["web app", "article", "Machine Learning"]
+    },
+    {
+       id: "5",
+       title: "My Portfolio Version 1",
+       category: "web app",
+       keyword: ["web app", "article", "Machine Learning"]
+    },
+    {
+       id: "6",
+       title: "My Portfolio Version 1",
+       category: "web app",
+       keyword: ["web app", "article", "Machine Learning"]
+    },
+]
+
+export default portfolio
